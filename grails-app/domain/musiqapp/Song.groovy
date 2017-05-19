237d980daa21b11c5a66a31f199967b054db25a2
Samuel Tokalakis <samtok@bluewin.ch>
2017-05-19 23:49:46 +0200

    Needed some more attributes in the database
    I also save artist, image URL and album to be able to show the data in
    the gsp files

--- grails-app/domain/musiqapp/Song.groovy
+++ grails-app/domain/musiqapp/Song.groovy
@@ -4,13 +4,18 @@ import grails.rest.Resource
 
 class Song {
     String name
+    String artist
     String songID
     User user
     Date dateAdded
+    String image
+    String album
 
     static belongsTo = [ party: Party ]
 
     static constraints = {
+    	user nullable: true
 
     }
+
 }
