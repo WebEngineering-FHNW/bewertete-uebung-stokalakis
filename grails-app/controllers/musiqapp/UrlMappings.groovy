75148efe541172fdcb8689a2cbe1ec9357e7fb0a
Samuel Tokalakis <samtok@bluewin.ch>
2017-05-19 23:48:38 +0200

    Changed default site
    Starting grails points now directly to MusiQ app

--- grails-app/controllers/musiqapp/UrlMappings.groovy
+++ grails-app/controllers/musiqapp/UrlMappings.groovy
@@ -8,8 +8,9 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-
-        "/"(view:"/index")
+		
+		"/"(view:"/welcome/index")
+        //"/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
